39f5abc542c69c0374292fb3cfa1fa67429c88f7
younus-always <younusofficial443@gmail.com>
2026-01-29 13:55:21 +0600

    created header, store-navbar and store-sidebar components

--- src/app/(public)/layout.jsx
+++ src/app/(public)/layout.jsx
@@ -1,14 +1,17 @@
 "use client";
+import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 
 export default function PublicLayout({ children }) {
       return (
-            <>
+            <div className="min-h-screen flex flex-col">
+                  <Header />
                   <Navbar />
-                  {/* <Banner /> */}
-                  {children}
+                  <main className="grow">
+                        {children}
+                  </main>
                   <Footer />
-            </>
+            </div>
       )
 }
