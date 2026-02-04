7b95932ad33349f8f1625fd3a45bbb428abe0c36
younus-always <younusofficial443@gmail.com>
2026-02-04 20:43:00 +0600

    created loading, pricing, and create-store page components

---
+++ src/app/(public)/pricing/page.jsx
@@ -0,0 +1,9 @@
+
+export default function PricingPage() {
+      return (
+            <div className='mx-auto max-w-175 my-28'>
+                  {/* Pricing Table */}
+
+            </div>
+      )
+}
