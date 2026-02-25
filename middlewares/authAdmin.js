18c06388fb6c72af11b82bfd5fb8245b800c6c8e
younus-always <younusofficial443@gmail.com>
2026-02-25 20:24:30 +0600

    created authAdmin middleware function

---
+++ middlewares/authAdmin.js
@@ -0,0 +1,15 @@
+import { clerkClient } from "@clerk/nextjs/dist/types/server";
+
+export const authAdmin = async (userId) => {
+      try {
+            if (!userId) return false;
+
+            const client = await clerkClient();
+            const user = await client.users.getUser(userId);
+
+            return process.env.ADMIN_EMAIL.split(",").includes(user.emailAddresses[0].emailAddress);
+      } catch (err) {
+            console.log(err);
+            return false;
+      }
+};
