a877a4df4c4e2494c98aaf4dc1350e50b164c3af
younus-always <younusofficial443@gmail.com>
2026-03-09 10:15:52 +0600

    lib: productSlice - fetchProducts function & extraReducers added

--- app/(public)/layout.jsx
+++ app/(public)/layout.jsx
@@ -2,8 +2,17 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchProducts } from "@/lib/features/product/productSlice";
 
 export default function PublicLayout({ children }) {
+      const dispatch = useDispatch();
+
+      useEffect(() => {
+            dispatch(fetchProducts({}));
+      }, []);
+
       return (
             <div className="min-h-screen flex flex-col">
                   <Header />
