67060ff432ecbc0e9611e2e4949ea8d30ac9a6da
younus-always <younusofficial443@gmail.com>
2026-03-10 12:10:00 +0600

    layout.jsx file modified

--- app/(public)/layout.jsx
+++ app/(public)/layout.jsx
@@ -5,13 +5,30 @@ import Navbar from "@/components/Navbar";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchProducts } from "@/lib/features/product/productSlice";
+import { useAuth, useUser } from "@clerk/nextjs";
+import { fetchCart, uploadCart } from "@/lib/features/cart/cartSlice";
 
 export default function PublicLayout({ children }) {
       const dispatch = useDispatch();
+      const { user } = useUser();
+      const { getToken } = useAuth();
+      const { cartItems } = useSelector(state => state.cart);
 
       useEffect(() => {
             dispatch(fetchProducts({}));
       }, []);
+
+      useEffect(() => {
+            if (user) {
+                  dispatch(fetchCart({ getToken }))
+            }
+      }, [user]);
+
+      useEffect(() => {
+            if (user) {
+                  dispatch(uploadCart({ getToken }))
+            }
+      }, [cartItems]);
 
       return (
             <div className="min-h-screen flex flex-col">
