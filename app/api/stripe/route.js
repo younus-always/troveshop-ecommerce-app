ef533266297cd7351cdc0164d33881ed97f58b26
younus-always <younusofficial443@gmail.com>
2026-03-19 09:47:23 +0600

    api: stripe POST api method created

---
+++ app/api/stripe/route.js
@@ -0,0 +1,73 @@
+import prisma from "@/lib/prisma";
+import { NextResponse } from "next/server";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+export const POST = async (request) => {
+      try {
+            const body = await request.text();
+            const sig = request.get("stripe-signature");
+            const event = stripe.webhooks.constructEvent(body, sig, process.env.STRIPE_WEBHOOK_SECRET);
+
+            const handlePaymentIntent = async (paymentIntentId, isPaid) => {
+                  const session = await stripe.checkout.sessions.list({
+                        payment_intent: paymentIntentId
+                  });
+                  const { orderIds, userId, appId } = session.data[0].metadata;
+
+                  if (appId !== "troveshop") {
+                        return NextResponse.json({ receive: true, message: "Invalid app id" });
+                  };
+
+                  const orderIdsArray = orderIds.split(",");
+
+                  if (isPaid) {
+                        // mark order as paid
+                        await Promise.all(orderIdsArray.map(async (orderId) => {
+                              await prisma.order.update({
+                                    where: { id: orderId },
+                                    data: { isPaid: true }
+                              });
+                        }));
+                        // delete cart from user
+                        await prisma.user.update({
+                              where: { id: userId },
+                              data: { cart: {} }
+                        });
+                  } else {
+                        // delete order from db
+                        await Promise.all(orderIdsArray.map(async (orderId) => {
+                              await prisma.order.delete({
+                                    where: { id: orderId }
+                              });
+                        }));
+                  };
+
+            };
+
+            switch (event.type) {
+                  case "payment-intent.succeeded": {
+                        await handlePaymentIntent(event.data.object.id, true);
+                        break;
+                  };
+
+                  case "payment-intent.canceled": {
+                        await handlePaymentIntent(event.data.object.id, false);
+                        break;
+                  };
+
+                  default:
+                        console.log("Unhandled event type:", event.type);
+                        break;
+            };
+
+            NextResponse.json({ receive: true });
+      } catch (err) {
+            console.error(err);
+            NextResponse.json({ error: err.message }, { status: 400 });
+      }
+};
+
+export const config = {
+      api: { bodyparser: false }
+};
