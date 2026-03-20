05e7036d2494d523d4eb425f6578bc213afd150b
younus-always <younusofficial443@gmail.com>
2026-03-20 10:29:18 +0600

    stripe webhooks route updated

--- app/api/stripe/route.js
+++ app/api/stripe/route.js
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 export const POST = async (request) => {
       try {
             const body = await request.text();
-            const sig = request.get("stripe-signature");
+            const sig = request.headers.get("stripe-signature");
             const event = stripe.webhooks.constructEvent(body, sig, process.env.STRIPE_WEBHOOK_SECRET);
 
             const handlePaymentIntent = async (paymentIntentId, isPaid) => {
